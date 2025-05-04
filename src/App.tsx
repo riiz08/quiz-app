7e03b9584a00c52a45ebf613759cf8aa25dcbc83
riiz08 <mhmdrizki0812@gmail.com>
2025-05-04 18:31:13 +0700

    add route the beekeeper 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import IndexPage from "@/pages/index";
 import QuizPage from "./pages/quiz";
 import ResultPage from "./pages/result";
 import { useAdsterra } from "./hooks/useAdsterra";
+import TheBeekeeper2 from "./pages/the-beekeeper-2";
 
 function App() {
   useAdsterra();
@@ -12,6 +13,7 @@ function App() {
       <Route element={<IndexPage />} path="/" />
       <Route element={<QuizPage />} path="/quiz" />
       <Route element={<ResultPage />} path="/result" />
+      <Route element={<TheBeekeeper2 />} path="/watch/the-beekeeper-2" />
     </Routes>
   );
 }
---
+++ src/pages/the-beekeeper-2.tsx
@@ -0,0 +1,82 @@
+import { Image } from "@heroui/image";
+import { PlayCircle } from "lucide-react";
+import { Helmet } from "react-helmet";
+
+const TheBeekeeper2 = () => {
+  return (
+    <>
+      <Helmet>
+        {/* Primary Meta Tags */}
+        <title>Watch The Beekeeper 2 online streaming or Download</title>
+        <meta
+          name="title"
+          content="Watch The Beekeeper 2 online streaming or Download"
+        />
+        <meta
+          name="description"
+          content="AI Overview
+Learn more
+There is no official information available about  the beekeeper 2 beyond speculation and fan theories. while the beekeeper (2024) was a success, there's no confirmed sequel or official synopsis yet. movie follows adam clay, former intelligence operative, who seeks revenge htmlFor death of friend after phishing scam. movie's success has led to about sequel, but details are confirmed"
+        />
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://quizdateyou.web.id/watch/the-beekeeper-2"
+        />
+        <meta
+          property="og:title"
+          content="Watch The Beekeeper 2 online streaming or Download"
+        />
+        <meta
+          property="og:description"
+          content="AI Overview
+Learn more
+There is no official information available about  the beekeeper 2 beyond speculation and fan theories. while the beekeeper (2024) was a success, there's no confirmed sequel or official synopsis yet. movie follows adam clay, former intelligence operative, who seeks revenge htmlFor death of friend after phishing scam. movie's success has led to about sequel, but details are confirmed"
+        />
+        <meta
+          property="og:image"
+          content="https://quizdateyou.web.id/images/png/tb2.jpg"
+        />
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://quizdateyou.web.id/watch/the-beekeeper-2"
+        />
+        <meta
+          property="twitter:title"
+          content="Watch The Beekeeper 2 online streaming or Download"
+        />
+        <meta
+          property="twitter:description"
+          content="AI Overview
+Learn more
+There is no official information available about  the beekeeper 2 beyond speculation and fan theories. while the beekeeper (2024) was a success, there's no confirmed sequel or official synopsis yet. movie follows adam clay, former intelligence operative, who seeks revenge htmlFor death of friend after phishing scam. movie's success has led to about sequel, but details are confirmed"
+        />
+        <meta
+          property="twitter:image"
+          content="https://quizdateyou.web.id/images/png/tb2.jpg"
+        />
+        {/* Meta Tags Generated with https://metatags.io */}
+      </Helmet>
+      <div className="flex justify-center items-center min-h-screen bg-[url(/images/png/tb2-bg.jpg)] ">
+        <div className="relative">
+          <Image src="/images/png/tb2.jpg" className="w-full" />
+          <div className="absolute w-full rounded-lg h-full top-0 z-10 bg-black opacity-45"></div>
+          <div className="absolute w-full rounded-lg h-full top-0 z-10 cursor-pointer">
+            <div className="flex justify-center items-center h-full">
+              <PlayCircle
+                className="bg-red-800 p-2 rounded-full"
+                size={50}
+                color="white"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TheBeekeeper2;
